2abe45e364742f4f6d1ec3eb0990cccb6f9d4861
Fernando Ibarra Moya <fibarra75.m@gmail.com>
2021-06-10 11:54:43 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppActiveDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ldapPath2 = "adnet2.cl";
+
+            try
+            {
+                LdapConnection connection = new LdapConnection(ldapPath2);
+                var credentials = new NetworkCredential(@"fibarra", "P2ssw0rd");
+                connection.Credential = credentials;
+                connection.Bind();
+            } catch (Exception e)
+            {
+                ;
+            }
+
+            bool authenticated = false;
+
+            try
+            {
+                ITI.FWI.Seguridad.Autentificacion.ActiveDirectory objAD = new ITI.FWI.Seguridad.Autentificacion.ActiveDirectory("LDAP://DC=adnet2.cl,DC=CL");
+                authenticated = objAD.ValidaUsuario("fibarra", "P2ssw0rd");
+            } catch (Exception ex)
+            {
+                ;
+            }
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("ConsoleAppActiveDirectory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppActiveDirectory")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("86338f14-9590-4bef-849c-6d7cf2c31702")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
